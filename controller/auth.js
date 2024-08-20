ffd1e88d507cd46d39be67dc60ad02138924e390
Amit Singh <amitsskingh@gmail.com>
2024-08-20 20:31:51 +0530

    update

--- controller/auth.js
+++ controller/auth.js
@@ -7,10 +7,10 @@ const setCookie = (res, token) => {
   res.cookie("token", token, {
     expires: expires,
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    domain: "https://amitskingh-textmate.netlify.app",
   })
+  // secure: true,
+  // sameSite: "none",
+  // domain: "https://amitskingh-textmate.netlify.app",
 }
 
 const register = asyncWrapper(async (req, res) => {
